21b96fed2b1f6af11a0ec3b3889aae247c5e0529
Rajveer Narang <70830990+Rajveer-Narang@users.noreply.github.com>
2021-03-31 17:32:00 +0530

    inital commit
    working on view 
    html js and css code available in index.html file present in  src j->java-> resources -> index.html

---
+++ Details form/Details-form/src/main/java/com/upgrad/Details/form/DetailsFormApplication.java
@@ -0,0 +1,13 @@
+package com.upgrad.Details.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailsFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetailsFormApplication.class, args);
+	}
+
+}
---
+++ Details form/Details-form/src/test/java/com/upgrad/Details/form/DetailsFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.Details.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailsFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
